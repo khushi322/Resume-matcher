95c5f767cfb378a316c2d1e40a4963c688e4f5fb
Khushi Shukla <shuklakhushi353@gmail.com>
2025-08-19 01:44:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -73,6 +73,16 @@ def fetch_job_description_from_url(url: str):
 
 
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # for testing, allow all
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 def root():
     return {"message": "Resume matcher is live!"}
